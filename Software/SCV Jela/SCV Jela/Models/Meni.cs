fc5f255043f1e90492a97a7115a91ed122a69345
Luka Kačarik <lkacarik22@foi.hr>
2024-05-31 18:41:18 +0200

    Implementiran RepozitorijMenija Stvorena klasa RepozitorijMenija i napisan kod za dohvaćanje menija iz baze podataka.

---
+++ Software/SCV Jela/SCV Jela/Models/Meni.cs
@@ -0,0 +1,19 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCV_Jela.Models
+{
+    internal class Meni
+    {
+        public int Id { get; set; }
+        public string Juha { get; set; }
+        public string Glavno { get; set; }
+        public string Prilog { get; set; }
+        public string Desert {  get; set; }
+
+    }
+}
---
+++ Software/SCV Jela/SCV Jela/Repositories/RepozitorijMenija.cs
@@ -0,0 +1,66 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SCV_Jela.Models;
+using System.Data.SqlClient;
+
+namespace SCV_Jela.Repositories
+{
+    internal class RepozitorijMenija
+    {
+        public static Meni GetMeni(int id)
+        {
+            Meni meni = null;
+            string sql = $"SELECT * FROM Meni WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                meni = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return meni;
+        }
+
+        public static List<Meni> GetStudents()
+        {
+            List<Meni> meniji = new List<Meni>();
+            string sql = "SELECT * FROM Meni";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Meni meni = CreateObject(reader);
+                meniji.Add(meni);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return meniji;
+        }
+
+        private static Meni CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["Id"].ToString());
+            string juha = reader["Juha"].ToString();
+            string glavno = reader["Glavno"].ToString();
+            string prilog = reader["Prilog"].ToString();
+            string desert = reader["Desert"].ToString();
+            var meni = new Meni
+            {
+                Id = id,
+                Juha = juha,
+                Glavno = glavno,
+                Prilog = prilog,
+                Desert = desert
+            };
+            return meni;
+        }
+
+
+    }
+}
