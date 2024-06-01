ff17ed234e64a0205cc7eb7ae7ea1656a1ca1b36
Luka Kačarik <lkacarik22@foi.hr>
2024-06-01 22:39:56 +0200

    Stvorena forma za promjenu sadržaja menija Dizajnirana forma za promjenu sadržaja menija bez funkcionalnosti

--- Software/SCV Jela/SCV Jela/FormaMeniji.cs
+++ Software/SCV Jela/SCV Jela/FormaMeniji.cs
@@ -71,7 +71,10 @@ namespace SCV_Jela
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            FormaPromjena formaPromjena = new FormaPromjena();
+            Hide();
+            formaPromjena.ShowDialog();
+            Close();
         }
 
         private void button3_Click(object sender, EventArgs e)
---
+++ Software/SCV Jela/SCV Jela/FormaPromjena.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SCV_Jela
+{
+    public partial class FormaPromjena : Form
+    {
+        public FormaPromjena()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FormaMeniji formaMeniji = new FormaMeniji();
+            Hide();
+            formaMeniji.ShowDialog();
+            Close();
+        }
+    }
+}
