44eada6b3d10b8c045e06e0c0fe3ecbeeefe89f2
Luka Kačarik <lkacarik22@foi.hr>
2024-06-01 19:40:51 +0200

    Izrađena forma za listu menija Izrađena forma za listu svih menija te postavljeni gumbi za operacije s listom

--- Software/SCV Jela/SCV Jela/FormaMeniji.Designer.cs
+++ Software/SCV Jela/SCV Jela/FormaMeniji.Designer.cs
@@ -28,39 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormaMeniji));
             this.dgvMeniji = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMeniji)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvMeniji
             // 
             this.dgvMeniji.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMeniji.Location = new System.Drawing.Point(12, 12);
+            this.dgvMeniji.Location = new System.Drawing.Point(19, 174);
+            this.dgvMeniji.Margin = new System.Windows.Forms.Padding(2);
             this.dgvMeniji.Name = "dgvMeniji";
             this.dgvMeniji.RowTemplate.Height = 24;
-            this.dgvMeniji.Size = new System.Drawing.Size(900, 577);
+            this.dgvMeniji.Size = new System.Drawing.Size(579, 365);
             this.dgvMeniji.TabIndex = 0;
             this.dgvMeniji.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMeniji_CellContentClick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(297, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 67);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(131, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 41);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lista dostupnih menija";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CadetBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(639, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Dodaj novi meni";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.CadetBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(639, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 71);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Promijeni sadržaj menija";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.CadetBlue;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(639, 494);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Izbriši meni";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.CadetBlue;
+            this.button4.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(639, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 45);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Pronađi meni";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
             // FormaMeniji
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(924, 601);
+            this.ClientSize = new System.Drawing.Size(902, 560);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dgvMeniji);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormaMeniji";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SCV Jela - Meniji";
             this.Load += new System.EventHandler(this.FormaMeniji_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMeniji)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvMeniji;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Software/SCV Jela/SCV Jela/FormaMeniji.cs
+++ Software/SCV Jela/SCV Jela/FormaMeniji.cs
@@ -33,6 +33,50 @@ namespace SCV_Jela
         {
             List<Meni> meni = RepozitorijMenija.GetMeni();
             dgvMeniji.DataSource = meni;
+
+            /*
+            List<Meni> meni = RepozitorijMenija.GetMeni();
+            List<Meni> odabraniMeniji = new List<Meni>
+            {
+                meni.ElementAt(0),
+                meni.ElementAt(1),
+                meni.ElementAt(2)
+            };
+            dgvMeni1.DataSource = odabraniMeniji;
+            
+            List<Meni> meni2 = RepozitorijMenija.GetMeni();
+            List<Meni> odabraniMeniji2 = new List<Meni>
+            {
+                meni.ElementAt(3),
+                meni.ElementAt(4),
+                meni.ElementAt(5)
+            };
+            dgvMeni2.DataSource = odabraniMeniji2;
+
+            List<Meni> meni3 = RepozitorijMenija.GetMeni();
+            List<Meni> odabraniMeniji3 = new List<Meni>
+            {
+                meni.ElementAt(6),
+                meni.ElementAt(7),
+                meni.ElementAt(8)
+            };
+            dgvMeni3.DataSource = odabraniMeniji3;
+            */
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
